9bc3701955b080a3bf42038329ee47b2fb35bd27
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-07-20 11:04:36 -0300

    criado menu mobile

--- 07-SunnysideLandingPage/script.js
+++ 07-SunnysideLandingPage/script.js
@@ -1,5 +1,29 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navItems = document.querySelectorAll(".nav--item");
 
-window.addEventListener("scroll", function() {
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const nav = document.querySelector(".header-nav");
+  nav.classList.toggle("ativo");
+  const ativo = nav.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
+}
+
+navItems.forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
+
+mobileMenu.addEventListener("click", toggleMenu);
+mobileMenu.addEventListener("touchstart", toggleMenu);
+
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
