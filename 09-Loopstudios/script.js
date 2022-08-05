2095841a4485d918bf1bb8f4910795988c39353c
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-08-05 12:31:50 -0300

    adicionado mobile-menu

--- 09-Loopstudios/script.js
+++ 09-Loopstudios/script.js
@@ -1,3 +1,29 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navItems = document.querySelectorAll(".header__nav-item");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const body = document.querySelector("body");
+  body.classList.toggle("ativo");
+  const ativo = body.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
+  if (ativo) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
+}
+
+navItems.forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
+
+mobileMenu.addEventListener("click", toggleMenu);
+mobileMenu.addEventListener("touchstart", toggleMenu);
+
+
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
