6103d2521c6723a871387c03dd1668fae3433162
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-08-17 16:09:02 -0300

    ajustes finais

--- 10-Insure/script.js
+++ 10-Insure/script.js
@@ -1,44 +1,47 @@
 const menu = document.querySelector("#mobile-menu");
 const navItem = document.querySelectorAll(".nav-header__item");
 const data = document.querySelectorAll("[data-aos]");
-
-function toggleMenu(event) {
-  if (event.type === "touchstart") {
-    event.preventDefault();
-  }
-  const body = document.querySelector("body");
-  body.classList.toggle("ativo");
-
-  const ativo = body.classList.contains("ativo");
-  event.currentTarget.setAttribute("aria-expanded", ativo);
-  if (ativo) {
-    event.currentTarget.setAttribute("aria-label", "Fechar menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir menu");
-  }
-}
-
-function animar() {
-  const windowTop = window.pageYOffset + window.innerHeight * 0.75;
-  data.forEach((element) => {
-    if (windowTop > element.offsetTop) {
-      element.classList.add("animacao");
-    } else {
-      element.classList.remove("animacao");
+const body = document.querySelector("body");
+body.classList.add("js");
+if (body.classList.contains("js")) {
+  function toggleMenu(event) {
+    if (event.type === "touchstart") {
+      event.preventDefault();
     }
+    body.classList.toggle("ativo");
+
+    const ativo = body.classList.contains("ativo");
+    event.currentTarget.setAttribute("aria-expanded", ativo);
+    if (ativo) {
+      event.currentTarget.setAttribute("aria-label", "Fechar menu");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "Abrir menu");
+    }
+  }
+
+  function animar() {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.75;
+    data.forEach((element) => {
+      if (windowTop > element.offsetTop) {
+        element.classList.add("animacao");
+      } else {
+        element.classList.remove("animacao");
+      }
+    });
+  }
+
+  animar();
+
+  navItem.forEach((link) => {
+    link.addEventListener("click", toggleMenu);
+  });
+
+  menu.addEventListener("click", toggleMenu);
+  menu.addEventListener("touchstart", toggleMenu);
+
+  window.addEventListener("scroll", () => {
+    animar();
+    let header = document.querySelector(".header");
+    header.classList.toggle("sticky", window.scrollY > 0);
   });
 }
-animar();
-
-navItem.forEach((link) => {
-  link.addEventListener("click", toggleMenu);
-});
-
-menu.addEventListener("click", toggleMenu);
-menu.addEventListener("touchstart", toggleMenu);
-
-window.addEventListener("scroll", () => {
-  animar();
-  let header = document.querySelector(".header");
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
